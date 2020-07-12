c0ad7d35b9edbad5aa8f368736fefd5762176bcc
heosang <57525618+heosang@users.noreply.github.com>
2020-07-13 01:43:13 +0900

    Update MenuItemRepository.java

--- eatgo-api/src/main/java/com/fastcampus/eatgo/domain/MenuItemRepository.java
+++ eatgo-api/src/main/java/com/fastcampus/eatgo/domain/MenuItemRepository.java
@@ -3,5 +3,5 @@ package com.fastcampus.eatgo.domain;
 import java.util.List;
 
 public interface MenuItemRepository {
-    List<MenuItem> findAllByRestaurantId(int restaurantId);
+    List<MenuItem> findAllByRestaurantId(long restaurantId);
 }
