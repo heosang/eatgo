43522275be07c824bcc579904db6cf44d8b9b731
heosang <57525618+heosang@users.noreply.github.com>
2020-07-13 01:43:18 +0900

    Update RestaurantController.java

--- eatgo-api/src/main/java/com/fastcampus/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/com/fastcampus/eatgo/interfaces/RestaurantController.java
@@ -1,9 +1,7 @@
 package com.fastcampus.eatgo.interfaces;
 
-import com.fastcampus.eatgo.domain.MenuItem;
-import com.fastcampus.eatgo.domain.MenuItemRepository;
+import com.fastcampus.eatgo.application.RestaurantService;
 import com.fastcampus.eatgo.domain.Restaurant;
-import com.fastcampus.eatgo.domain.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,23 +12,25 @@ import java.util.List;
 @RestController
 public class RestaurantController {
 
+
     @Autowired
-    private RestaurantRepository restaurantRepository;
-    @Autowired
-    private MenuItemRepository menuItemRepository;
+    private RestaurantService restaurantService;
 
     @GetMapping("/restaurants")
     public List<Restaurant> list(){
-        List<Restaurant> restaurants = restaurantRepository.findAll();
+        List<Restaurant> restaurants = restaurantService.getRestaurants();
 
         return restaurants;
     }
     @GetMapping("/restaurants/{id}")
-    public Restaurant detail(@PathVariable("id") int id){
-        Restaurant restaurant = restaurantRepository.findById(id);
+    public Restaurant detail(@PathVariable("id") long id){
+        Restaurant restaurant = restaurantService.getRestaurant(id);
+        //기본정보 + 메뉴정보
+
+       /* Restaurant restaurant = restaurantRepository.findById(id);
 
         List<MenuItem> menuItems=menuItemRepository.findAllByRestaurantId(id);
-        restaurant.setMenuItems(menuItems);
+        restaurant.setMenuItems(menuItems);*/
         return restaurant;
     }
 }
