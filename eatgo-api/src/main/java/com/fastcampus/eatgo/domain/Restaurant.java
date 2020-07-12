4d7bb96f7e6fdeb7fedfa53423de023b2b073be0
heosang <57525618+heosang@users.noreply.github.com>
2020-07-12 22:23:59 +0900

    Update Restaurant.java

--- eatgo-api/src/main/java/com/fastcampus/eatgo/domain/Restaurant.java
+++ eatgo-api/src/main/java/com/fastcampus/eatgo/domain/Restaurant.java
@@ -1,9 +1,13 @@
 package com.fastcampus.eatgo.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Restaurant {
     private final String name;
     private final String location;
     private final int id;
+    private List<MenuItem> menuItems = new ArrayList<com.fastcampus.eatgo.domain.MenuItem>();
 
     public Restaurant(int id, String name, String location){
         this.id=id;
@@ -25,5 +29,18 @@ public class Restaurant {
 
     public int getId() {
         return id;
+    }
+    public List<com.fastcampus.eatgo.domain.MenuItem> getMenuItems(){
+        return menuItems;
+    }
+
+    public void addMenuItem(com.fastcampus.eatgo.domain.MenuItem menuItem) {
+        menuItems.add(menuItem);
+    }
+
+    public void setMenuItems(List<com.fastcampus.eatgo.domain.MenuItem> menuItems) {
+        for(com.fastcampus.eatgo.domain.MenuItem menuItem: menuItems){
+            addMenuItem(menuItem);
+        }
     }
 }
