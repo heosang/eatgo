14d81aebbc28e9804b539c9744409c8db287ff96
heosang <57525618+heosang@users.noreply.github.com>
2020-07-13 01:43:17 +0900

    Update RestaurantTest.java

--- eatgo-api/src/test/java/com/fastcampus/eatgo/domain/RestaurantTest.java
+++ eatgo-api/src/test/java/com/fastcampus/eatgo/domain/RestaurantTest.java
@@ -8,14 +8,14 @@ import static org.hamcrest.core.Is.is;
 class RestaurantTest {
     @Test
     public void creation(){
-        Restaurant restaurant=new Restaurant(1004,"Bob","Seoul");
-        assertThat(restaurant.getId(), is(1004));
+        Restaurant restaurant=new Restaurant(1004L,"Bob","Seoul");
+        assertThat(restaurant.getId(), is(1004L));
         assertThat(restaurant.getName(), is("Bob"));
         assertThat(restaurant.getLocation(),is("Seoul"));
     }
     @Test
     public void information(){
-        Restaurant restaurant=new Restaurant(1004, "Bob", "Seoul");
+        Restaurant restaurant=new Restaurant(1004L, "Bob", "Seoul");
         assertThat(restaurant.getInformation(), is("Bob in Seoul"));
     }
 
