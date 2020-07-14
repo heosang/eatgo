897949bd0cb1cb30174f6f26ecd15c8d3c7c97af
heosang <57525618+heosang@users.noreply.github.com>
2020-07-14 17:14:42 +0900

    Update RestaurantControllerTest.java

--- eatgo-api/src/test/java/com/fastcampus/eatgo/interfaces/RestaurantControllerTest.java
+++ eatgo-api/src/test/java/com/fastcampus/eatgo/interfaces/RestaurantControllerTest.java
@@ -1,22 +1,27 @@
 package com.fastcampus.eatgo.interfaces;
 
 import com.fastcampus.eatgo.application.RestaurantService;
-import com.fastcampus.eatgo.domain.MenuItemRepository;
-import com.fastcampus.eatgo.domain.MenuItemRepositoryImpl;
-import com.fastcampus.eatgo.domain.RestaurantRepository;
-import com.fastcampus.eatgo.domain.RestaurantRepositoryImpl;
+import com.fastcampus.eatgo.domain.MenuItem;
+import com.fastcampus.eatgo.domain.Restaurant;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @RunWith(SpringRunner.class)
@@ -25,39 +30,60 @@ class RestaurantControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    @SpyBean(RestaurantService.class)
+    @MockBean
     private RestaurantService restaurantService;
-    @SpyBean(RestaurantRepositoryImpl.class)
-    private RestaurantRepository restaurantRepository;
-
-    @SpyBean(MenuItemRepositoryImpl.class)
-    private MenuItemRepository menuItemRepository;
 
     @Test
     public void list() throws Exception {
+        List<Restaurant> restaurants = new ArrayList<>();
+        restaurants.add(new Restaurant(1004L,"house","Seoul"));
+        given(restaurantService.getRestaurants()).willReturn(restaurants);
+
         mvc.perform(get("/restaurants"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(
                         containsString("\"id\":1004")))
                 .andExpect(content().string(
-                containsString("\"name\":\"Bob\"")))
-                .andExpect(content().string(
-                        containsString("Kimchi")
-                ));
+                containsString("\"name\":\"house\"")));
     }
     @Test
     public void detail() throws Exception {
+        Restaurant restaurant1= new Restaurant(1004L,"house","Seoul");;
+        Restaurant restaurant2= new Restaurant(2020L,"CyberFood","Seoul");;
+        restaurant2.addMenuItem(new MenuItem("Kimchi"));
+
+        given(restaurantService.getRestaurant(1004L)).willReturn(restaurant1);
+        given(restaurantService.getRestaurant(2020L)).willReturn(restaurant2);
+
         mvc.perform(get("/restaurants/1004"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(
                         containsString("\"id\":1004")))
                 .andExpect(content().string(
-                        containsString("\"name\":\"Bob\"")));
+                        containsString("\"name\":\"house\"")));
         mvc.perform(get("/restaurants/2020"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(
                         containsString("\"id\":2020")))
                 .andExpect(content().string(
-                        containsString("\"name\":\"CyberFood\"")));
+                        containsString("\"name\":\"CyberFood\"")))
+                .andExpect(content().string(
+                        containsString("Kimchi")));
+    }
+
+
+
+    @Test
+    public void create() throws Exception {
+        Restaurant restaurant=new Restaurant(1234L,"Rong","Busan");
+
+        mvc.perform(post("/restaurants")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\":\"Rong\",\"address\":\"Busan\"}"))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("location","/restaurants/1234"))
+                .andExpect(content().string("{}"));
+        verify(restaurantService).addRestaurant(any());
+
     }
 }
