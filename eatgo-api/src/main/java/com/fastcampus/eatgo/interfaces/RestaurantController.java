13addf803402c6ac392fa3e8112cc3750e37839b
heosang <57525618+heosang@users.noreply.github.com>
2020-07-14 17:14:46 +0900

    Update RestaurantController.java

--- eatgo-api/src/main/java/com/fastcampus/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/com/fastcampus/eatgo/interfaces/RestaurantController.java
@@ -3,10 +3,11 @@ package com.fastcampus.eatgo.interfaces;
 import com.fastcampus.eatgo.application.RestaurantService;
 import com.fastcampus.eatgo.domain.Restaurant;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
@@ -26,11 +27,17 @@ public class RestaurantController {
     public Restaurant detail(@PathVariable("id") long id){
         Restaurant restaurant = restaurantService.getRestaurant(id);
         //기본정보 + 메뉴정보
-
-       /* Restaurant restaurant = restaurantRepository.findById(id);
-
-        List<MenuItem> menuItems=menuItemRepository.findAllByRestaurantId(id);
-        restaurant.setMenuItems(menuItems);*/
         return restaurant;
+    }
+    @PostMapping("/restaurants")
+    public ResponseEntity<?> create(@RequestBody Restaurant resource)
+            throws URISyntaxException {
+        String name = resource.getName();
+        String address = resource.getAddress();
+
+        Restaurant restaurant=new Restaurant(1234L, name, address);
+        restaurantService.addRestaurant(restaurant);
+        URI location= new URI("/restaurants/"+restaurant.getId());
+        return ResponseEntity.created(location).body("{}");
     }
 }
