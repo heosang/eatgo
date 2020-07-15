310ee1b26edea76925a729d5aae961da0d71d2db
heosang <57525618+heosang@users.noreply.github.com>
2020-07-15 11:44:39 +0900

    Update MenuItem.java

--- eatgo-api/src/main/java/com/fastcampus/eatgo/domain/MenuItem.java
+++ eatgo-api/src/main/java/com/fastcampus/eatgo/domain/MenuItem.java
@@ -1,7 +1,13 @@
 package com.fastcampus.eatgo.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
 public class MenuItem {
+    @Id
+    private long id;
+    private long restaurantId;
     private final String name;
 
     public MenuItem(String name){
